a9c797207fbe69638e572443db036af14176e75d
NaelAborrob <aborrob@me.com>
2019-04-11 18:37:55 +0100

    Added Buttons to switch graphs

--- script.js
+++ script.js
@@ -1,28 +1,72 @@
-var v12_button = document.getElementById("v12");
 
-v12_button.onclick = function () {
-    if (v12_button.className == "v12-off button") {
-        v12_button.className = "v12-on button";
-        v12_button.innerHTML = "v12 on";
-    } else {
-        v12_button.className = "v12-off button";
-        v12_button.innerHTML = "v12 off";
-    }
-
-};
 
 $(document).ready(function () {
+    // Button elements
+    var v12_button = document.getElementById("v12");
+    var BusOutCutrrentB = document.getElementById("BusOutCurrent");
+    var BusOutPowerInputB = document.getElementById("BusOutPowerInput");
+    var BusOutPowerDrainB = document.getElementById("BusOutPowerDrain");
+    var EnergyInGenerated = document.getElementById("EnergyInGenerated");
+    var EnergyOutGeneratedB = document.getElementById("EnergyOutGenerated");
+    var BusInPowerInputB = document.getElementById("BusInPowerInput");
+    var BusInPowerDrainB = document.getElementById("BusInPowerDrain");
+    var BusInCurrentB = document.getElementById("BusInCurrent");
+    // _________________________________^^^^^^^Buttons ^^^^^^___________________
     var ccObject = {
         'BusVoltage': 0,
-        'BusInCurrent': 0
+        'BusInCurrent': 0,
+        'BusOutCurrent': 0
     };
-
+    var ValueToBePlotted = ccObject.BusInCurrent;
+    var plotText = "Bus In Current";
     // Consider changing the interpolation (animation smoothness) to 'bezier', or just delete the 'step' and interpolation key value pair
     var smoothie1 = new SmoothieChart({ minValue: 0, title: { text: 'Bus Voltage', fillStyle: '#ffffff', fontSize: 15, fontFamily: 'sans-serif', verticalAlign: 'top' } });
-    var smoothie2 = new SmoothieChart({ minValue: 0, title: { text: 'Bus In Current', fillStyle: '#ffffff', fontSize: 15, fontFamily: 'sans-serif', verticalAlign: 'top' } });
+    var smoothie2 = new SmoothieChart({ minValue: 0, title: { text: plotText, fillStyle: '#ffffff', fontSize: 15, fontFamily: 'sans-serif', verticalAlign: 'top' } });
+    // _________________________________\/ \/ \/On Click \/ \/ \/ _______________
 
+    BusOutCutrrentB.onclick = function () {
+        console.log(this.value);
+        if (BusOutCutrrentB.value == 1) {
+            BusOutCutrrentB.value = 0;
+            ValueToBePlotted = ccObject.BusInCurrent;
+            plotText = "Bus In Current";
+            smoothie2.options.title.text = plotText;
+        }
+        else {
+            BusOutCutrrentB.value = 1;
+            ValueToBePlotted = ccObject.BusOutCurrent;
+            plotText = "Bus Out Current";
+            smoothie2.options.title.text = plotText;
+
+        }
+
+    };
+    // BusOutPowerInputB
+    // BusOutPowerDrainB
+    // EnergyInGenerated
+    // EnergyOutGeneratedB
+    // BusInPowerInputB
+    // BusInPowerDrainB
+    // BusInCurrentB
+
+    // _________________________________________/\/\/\/\/\On click /\/\/\/\/\_________
+
+    v12_button.onclick = function () {
+        if (v12_button.className == "v12-off button") {
+            v12_button.className = "v12-on button";
+            v12_button.innerHTML = "v12 on";
+        } else {
+            v12_button.className = "v12-off button";
+            v12_button.innerHTML = "v12 off";
+        }
+
+    };
+
+
+    // ___________________________Stream To Canvas
     smoothie1.streamTo(document.getElementById("mycanvas1"));
     smoothie2.streamTo(document.getElementById("mycanvas2"));
+    // smoothie2.streamTo(document.getElementById("mycanvas2"));
     // Data
     var line1 = new TimeSeries();
     var line2 = new TimeSeries();
@@ -36,7 +80,7 @@ $(document).ready(function () {
             success: function (data) {
                 ccObject = data;
                 line1.append(new Date().getTime(), ccObject.BusVoltage);
-                line2.append(new Date().getTime(), ccObject.BusInCurrent);
+                line2.append(new Date().getTime(), ValueToBePlotted);
                 // $('#testField').text(ccObject.CreateLog);
                 $('#CreateCurrentValue').text("Current Value: " + ccObject.CreateLog);
                 $('#OpenCurrentValue').text("Current Value: " + ccObject.OpenLog);
