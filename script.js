6f453bc543f4f1a51803fef9e082f8110afeaa57
NaelAborrob <aborrob@me.com>
2019-04-17 00:09:46 +0100

    Added activity states

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         }
         return i;
     }
-    
+
     function startTime() {
         var today = new Date();
         var h = today.getHours();
@@ -76,42 +76,53 @@ $(document).ready(function () {
     var start_time = new Date().getTime();
     var interval = null;
     var intervalDuration = 100;
-    interval = setInterval(function () {
-        start_time = new Date().getTime();
-        $.ajax({
-            url: "Data.json",
-            dataType: 'json',
-            type: "get",
-            cache: false,
-            success: function (data) {
-                ccObject = data;
-                var request_time = new Date().getTime() - start_time;
-                document.querySelector("#RequestTime").textContent = `Request time: ${request_time}`;
-                // console.log(request_time);
-            }
-        });
-        line1.append(new Date().getTime(), ccObject.BusVoltage);
-        line2.append(new Date().getTime(), ValueToBePlotted);
-        // console.log(ccObject);
-        $('#CreateCurrentValue').text("Current Value: " + ccObject.CreateLog);
-        $('#OpenCurrentValue').text("Current Value: " + ccObject.OpenLog);
-        $('#ClearCurrentValue').text("Current Value: " + ccObject.ClearLog);
-        $('#DeleteCurrentValue').text("Current Value: " + ccObject.DeleteLog);
-        $('#WritingCommandCurrentValue').text("Current Value: " + ccObject.StartWriteLog);
-        // if (ccObject.ReadTrig == 0) {
-        //     $('#ReadingCurrentValue').text("Status: Reading is OFF");
-        // } else {
-        //     $('#ReadingCurrentValue').text("Status: Reading is ON");
-        // }
-        // Data Address and Data length Control
-        $('#startAddressCurrentValue').text("Current Value: " + ccObject.StartAddress);
-        // Timers Control for reading Modbus data (Req freq.)
-        $('#TestPeriodCurrentValue').text("Current Value: " + ccObject.TestPeriod);
-        $('#DutyCycleCurrentValue').text("Current Value: " + ccObject.DutyCycle);
-    }, intervalDuration);
+    var intervalState = false;
+    function intervalManager() {
+        interval = setInterval(function () {
+            start_time = new Date().getTime();
+            $.ajax({
+                url: "Data.json",
+                dataType: 'json',
+                type: "get",
+                cache: false,
+                success: function (data) {
+                    ccObject = data;
+                    var request_time = new Date().getTime() - start_time;
+                    document.querySelector("#RequestTime").textContent = `Request time: ${request_time}`;
+                    // console.log(request_time);
+                }
+            });
+            line1.append(new Date().getTime(), ccObject.BusVoltage);
+            line2.append(new Date().getTime(), ValueToBePlotted);
+            // console.log(ccObject);
+            $('#CreateCurrentValue').text("Current Value: " + ccObject.CreateLog);
+            $('#OpenCurrentValue').text("Current Value: " + ccObject.OpenLog);
+            $('#ClearCurrentValue').text("Current Value: " + ccObject.ClearLog);
+            $('#DeleteCurrentValue').text("Current Value: " + ccObject.DeleteLog);
+            $('#WritingCommandCurrentValue').text("Current Value: " + ccObject.StartWriteLog);
+            // if (ccObject.ReadTrig == 0) {
+            //     $('#ReadingCurrentValue').text("Status: Reading is OFF");
+            // } else {
+            //     $('#ReadingCurrentValue').text("Status: Reading is ON");
+            // }
+            // Data Address and Data length Control
+            $('#startAddressCurrentValue').text("Current Value: " + ccObject.StartAddress);
+            // Timers Control for reading Modbus data (Req freq.)
+            $('#TestPeriodCurrentValue').text("Current Value: " + ccObject.TestPeriod);
+            $('#DutyCycleCurrentValue').text("Current Value: " + ccObject.DutyCycle);
+        }, intervalDuration);
+    }
+
     // __________Stop interval when pressed
-    document.querySelector("#ClearInterval").onclick = () => clearInterval(interval);
-    document.querySelector("#StartInterval").onclick = () => { intervalDuration = document.querySelector("#intervalInput").value; interval() };
+    document.querySelector("#ClearInterval").onclick = function () {
+        clearInterval(interval);
+        intervalState = false;
+    }
+    document.querySelector("#StartInterval").onclick = function () {
+        clearInterval(interval);
+        intervalDuration = document.querySelector("#intervalInput").value;
+        intervalManager();
+    }
 
     $("#LogCreateButton").click(function () {
         url = "Outputs.htm";
@@ -120,6 +131,7 @@ $(document).ready(function () {
         sdata = escape(name) + '=' + val;
         $.post(url, sdata, function (result) { });
     });
+
     $("#LogOpenButton").click(function () {
         url = "Outputs.htm";
         name = 'DB16.DBX0.1';
@@ -127,6 +139,7 @@ $(document).ready(function () {
         sdata = escape(name) + '=' + val;
         $.post(url, sdata, function (result) { });
     });
+
     $("#LogClearButton").click(function () {
         url = "Outputs.htm";
         name = 'DB16.DBX0.2';
@@ -134,6 +147,7 @@ $(document).ready(function () {
         sdata = escape(name) + '=' + val;
         $.post(url, sdata, function (result) { });
     });
+
     $("#LogDeleteButton").click(function () {
         url = "Outputs.htm";
         name = 'DB16.DBX0.3';
@@ -141,6 +155,7 @@ $(document).ready(function () {
         sdata = escape(name) + '=' + val;
         $.post(url, sdata, function (result) { });
     });
+
     $("#LogWriteButton").click(function () {
         url = "Outputs.htm";
         name = 'DB16.DBX0.5';
@@ -148,6 +163,7 @@ $(document).ready(function () {
         sdata = escape(name) + '=' + val;
         $.post(url, sdata, function (result) { });
     });
+
     // $("#StartReadButton").click(function () {
     //     url = "Outputs.htm";
     //     name = 'DB16.DBX0.4';
@@ -208,7 +224,27 @@ $(document).ready(function () {
             // e.target.classList.toggle("hidden");
         }
     }
+    var TestInfo = {
+        title: "",
+        SerialNmber: "",
+        Description: ""
+    }
 
-
+    document.querySelector(".TestInfo button").onclick = function (e) {
+        TestInfo.title = e.target.parentElement.children[2].value;
+        TestInfo.SerialNmber = e.target.parentElement.children[4].value;
+        TestInfo.Description = e.target.parentElement.children[6].value;
+        console.log(TestInfo);
+        document.querySelector("#testTitle").innerHTML = TestInfo.title;
+        document.querySelector("#testSerial").innerHTML = TestInfo.SerialNmber;
+        document.querySelector("#testDescription").innerHTML = TestInfo.Description;
+        document.querySelector(".TestInfo fieldset").classList.add("hidden");
+        document.querySelector(".submitted").classList.remove("hidden");
+    }
+    document.querySelector(".submitted button").onclick = function (e) {
+        console.log(e.target.parentElement);
+        document.querySelector(".TestInfo fieldset").classList.remove("hidden");
+        e.target.parentElement.classList.add("hidden");
+    }
 });
 // _____Don't add below this line to stay inside the onready scope!!!_________
